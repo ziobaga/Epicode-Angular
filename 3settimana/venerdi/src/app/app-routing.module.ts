6c1a08eae5abfd695295fe3356666c75da0c1cd5
ziobaga <adrianoleanza01@gmail.com>
2024-06-07 17:05:24 +0200

    okk

--- 3settimana/venerdi/src/app/app-routing.module.ts
+++ 3settimana/venerdi/src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestGuard } from './auth/guest.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [GuestGuard],
+    canActivateChild: [GuestGuard],
   },
   {
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canActivate: [GuestGuard],
+    canActivateChild: [GuestGuard],
   },
   {
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
   },
 ];
 
